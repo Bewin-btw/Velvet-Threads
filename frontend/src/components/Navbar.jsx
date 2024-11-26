bbf5d637f60646d2484f37b51f579fc068b4c9ab
Bewin-btw <aktaeverikwork@gmail.com>
2024-11-26 17:10:12 +0500

    front-end done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../Context/ShopContext';
+import { useContext, useState } from 'react';
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false)
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 front-medium'>
@@ -50,7 +53,7 @@ const Navbar = () => {
             </div>
             <Link to='/cart' className='relative'>
             <img src={assets.cart_icon} className='w-5 min-w-5' alt='' />
-            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]' >4</p>
+            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]' >{getCartCount()}</p>
             </Link>
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt='' />
         </div>
@@ -74,3 +77,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
